1e8a8c40b43c546d704d5457be76ab5d55d58eb2
Paulav Mukherjee <paulavmukherjee@gmail.com>
2023-05-27 22:29:23 +0530

    Done

--- src/app/home/page/page.component.ts
+++ src/app/home/page/page.component.ts
@@ -20,14 +20,14 @@ export class PageComponent implements OnInit, DoCheck {
       this.updateClockHands();
     }, 1000);
 
-    
+
   }
-ngDoCheck(): void {
-  let user = localStorage.getItem('userActive')
+  ngDoCheck(): void {
+    let user = localStorage.getItem('userActive')
     if (user === null) {
       this.isUser = true
     }
-}
+  }
 
 
   updateClockHands() {
@@ -39,8 +39,48 @@ ngDoCheck(): void {
     const hourRotation = (hours % 12) * 30 + (minutes / 2);
     const minuteRotation = minutes * 6;
     const secondRotation = seconds * 6;
+    let month = now.getMonth().toString()
+    //find month
+    switch (month) {
+      case '0':
+        month = 'January'
+        break
+      case '1':
+        month = 'February'
+        break
+      case '2':
+        month = 'March'
+        break
+      case '3':
+        month = 'April'
+        break
+      case '4':
+        month = 'May'
+        break
+      case '5':
+        month = 'June'
+        break
+      case '6':
+        month = 'July'
+        break
+      case '7':
+        month = 'Augest'
+        break
+      case '8':
+        month = 'September'
+        break
+      case '9':
+        month = 'October'
+        break
+      case '10':
+        month = 'November'
+        break
+      case '11':
+        month = 'December'
+        break
+    }
 
-    this.currentDate = now.getDate().toString()+ " - "+ now.getMonth()+ " - " + now.getFullYear()
+    this.currentDate = now.getDate().toString() + " - " + month + " - " + now.getFullYear()
     this.hourHandTransform = `rotate(${hourRotation}deg)`;
     this.minuteHandTransform = `rotate(${minuteRotation}deg)`;
     this.secondHandTransform = `rotate(${secondRotation}deg)`;
